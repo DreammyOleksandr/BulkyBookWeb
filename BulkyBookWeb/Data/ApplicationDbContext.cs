bcc6006f7be926d62f9d7017037be7c9151f71f9
bondarenkooleksandr <albom2004q@gmail.com>
2023-01-12 10:49:25 +0200

    Adding Db Context

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseSerialColumns();
+    }
+    public DbSet<Category> Categories { get; set; }
+}
