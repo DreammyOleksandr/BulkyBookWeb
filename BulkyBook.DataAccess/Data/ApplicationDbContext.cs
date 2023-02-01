abc8162f285013ae7202e39f9123acfb2a62b73c
Alex Bondarenko <albom2004q@gmail.com>
2023-02-01 15:40:37 +0200

    Adding N-Tier Architecture to the project

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,18 +1,18 @@
-using BulkyBookWeb.Models;
+using BulkyBook.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyBookWeb.Data;
+namespace BulkyBook.DataAccess;
 
 public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseSerialColumns();
     }
+
     public DbSet<Category> Categories { get; set; }
 }
