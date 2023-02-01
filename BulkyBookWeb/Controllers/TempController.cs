f7fe2e5b99846cfdab13ccf236da74cbde0572cf
Alex Bondarenko <albom2004q@gmail.com>
2023-02-01 15:40:37 +0200

    Adding N-Tier Architecture to the project

--- BulkyBookWeb/Controllers/TempController.cs
+++ BulkyBookWeb/Controllers/TempController.cs
@@ -45,10 +45,6 @@ namespace BulkyBookWeb.Controllers
         {
             return View();
         }
-
-        // POST: Temp/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,DisplayOrder,CreationTime")] Category category)
