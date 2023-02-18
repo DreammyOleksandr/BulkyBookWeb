19750be0c9f4be9dbc146afc8f9fb800eb6b5c04
Alex Bondarenko <albom2004q@gmail.com>
2023-02-18 19:54:54 +0200

    Making ProductVM be able to store to the database

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -10,18 +10,19 @@ namespace BulkyBookWeb.Controllers;
 public class ProductController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IWebHostEnvironment _hostEnvironment;
 
-    public ProductController(IUnitOfWork unitOfWork)
+    public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
     {
         _unitOfWork = unitOfWork;
+        _hostEnvironment = hostEnvironment;
     }
 
     public IActionResult Index()
     {
-        IEnumerable<Product> objectOfProductList = _unitOfWork.Product.GetAll();
-        return View(objectOfProductList);
+        return View();
     }
-    
+
     public IActionResult Upsert(int? Id)
     {
         ProductVM productVm = new()
@@ -37,19 +38,48 @@ public class ProductController : Controller
                 Text = i.Name,
                 Value = i.Id.ToString()
             }),
-
         };
         if (Id == null || Id == 0)
         {
             // ViewBag.CategoryList = categoryList;
             // ViewData["CoverTypeList"] = coverTypeList;
-            return View(productVm); 
+            return View(productVm);
         }
         else
         {
         }
 
         return View(productVm);
+    }
+
+    [HttpPost]
+    [AutoValidateAntiforgeryToken]
+    public IActionResult Upsert(ProductVM obj, IFormFile file)
+    {
+        if (ModelState.IsValid)
+        {
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            if (file != null)
+            {
+                string fileName = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(wwwRootPath, @"images/products");
+                var extension = Path.GetExtension(file.FileName);
+
+                using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                {
+                    file.CopyTo(fileStreams);
+                }
+
+                obj.Product.ImageURL = @"\images\products\" + fileName + extension;
+            }
+
+            _unitOfWork.Product.Add(obj.Product);
+            _unitOfWork.Save();
+            TempData["success"] = "Successful creation";
+            return RedirectToAction("Index");
+        }
+        else
+            return View(obj);
     }
 
     [HttpPost]
@@ -67,4 +97,15 @@ public class ProductController : Controller
         TempData["success"] = "Successful deletion";
         return RedirectToAction("Index");
     }
+
+    #region API CALLS
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var productList = _unitOfWork.Product.GetAll();
+        return Json(new { data = productList });
+    }
+
+    #endregion
 }
