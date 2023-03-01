63952d9b79528708248c36b13967874c4ca2375d
Alex Bondarenko <albom2004q@gmail.com>
2023-03-01 17:20:37 +0200

    Rendering HTML in Databases

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -14,7 +14,22 @@ function loadDatatable() {
             {"data": "isbn", "width": "15%"},
             {"data": "pricePerBook", "width": "15%"},
             {"data": "author", "width": "15%"},
-            // {"data": "Title", "width": "15%"},
+            {"data": "category.name", "width": "15%"},
+            {
+                "data": "id",
+                "render": function (data){
+                    return`
+                    <div class="w-75 btn-group" role="group">
+                    <a href="/Admin/Product/Upsert?id=${data}"
+                     class="btn btn-primary mx-2 ">
+                        <i class="bi bi-pencil"></i>EDIT</a>
+                    <a class="btn btn-primary mx-2 ">
+                        <i class="bi bi-trash"></i>DELETE</a>
+                </div>
+                    `  
+                },
+                "width": "15%"
+            }, 
         ]
     });
 }
